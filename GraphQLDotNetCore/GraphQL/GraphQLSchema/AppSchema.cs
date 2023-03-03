4a69e046fec9af6fdcd2c3e27aa6b68984074c93
Ravi Ranjan <ravi.ranjan2@infinite.com>
2023-03-03 11:12:38 +0530

    Add project files.

---
+++ GraphQLDotNetCore/GraphQL/GraphQLSchema/AppSchema.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using GraphQL.Utilities;
+using GraphQLDotNetCore.GraphQL.GraphQLQueries;
+using System;
+
+namespace GraphQLDotNetCore.GraphQL.GraphQLSchema
+{
+    public class AppSchema : Schema
+    {
+        public AppSchema(IServiceProvider provider)
+            :base(provider)
+        {
+            Query = provider.GetRequiredService<AppQuery>();
+        }
+    }
+}
